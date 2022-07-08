7d014af8d33f917c5f12a826a8fc41741c5682e9
Thiago Henrique Maçola <5by5.thenrique@voeazul.com.br>
2022-07-08 14:53:24 -0300

    projeto

---
+++ SistemaCompra.Application/SolicitacaoCompra/Command/RegistrarCompra/RegistrarCompraCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace SistemaCompra.Application.SolicitacaoCompra.Command.RegistrarCompra
+{
+	public class RegistrarCompraCommand : IRequest<bool>
+	{
+		public string UsuarioSolicitante { get; private set; }
+		public string NomeFornecedor { get; private set; }
+		public IList<ItemCommand> Itens { get; private set; }
+		public DateTime Data { get; private set; }
+
+	}
+	public partial class ItemCommand
+	{
+		public ProdutoCommand Produto { get; set; }
+		public int Quantidade { get; set; }
+
+	}
+
+	public partial class ProdutoCommand
+	{
+		public string Nome { get; set; }
+		public string Categoria { get; set; }
+		public string Descricao { get; set; }
+		public decimal Preco { get; set; }
+
+	}
+}
---
+++ SistemaCompra.Application/SolicitacaoCompra/Command/RegistrarCompra/RegistrarCompraCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using SistemaCompra.Domain.SolicitacaoCompraAggregate;
+using SistemaCompra.Infra.Data.UoW;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SistemaCompra.Application.SolicitacaoCompra.Command.RegistrarCompra
+{
+	public class RegistrarCompraCommandHandler : CommandHandler, IRequestHandler<RegistrarCompraCommand, bool>
+	{
+		private readonly ISolicitacaoCompraRepository _solicitacaoCompraRepository;
+
+		public RegistrarCompraCommandHandler(ISolicitacaoCompraRepository solicitacaoCompraRepository, IUnitOfWork uow, IMediator mediator) : base(uow, mediator)
+		{
+			solicitacaoCompraRepository = _solicitacaoCompraRepository;
+		}
+
+		public Task<bool> Handle(RegistrarCompraCommand request, CancellationToken cancellationToken)
+		{
+			var solicitacaoCompra = new Domain.SolicitacaoCompraAggregate.SolicitacaoCompra(request.NomeFornecedor, request.UsuarioSolicitante);
+			foreach (var item in request.Itens)
+			{
+				solicitacaoCompra.AdicionarItem(new Domain.ProdutoAggregate.Produto(item.Produto.Nome,item.Produto.Descricao, item.Produto.Categoria, item.Produto.Preco), item.Quantidade);
+			}
+			solicitacaoCompra.RegistrarCompra(solicitacaoCompra.Itens);
+			_solicitacaoCompraRepository.RegistrarCompra(solicitacaoCompra);
+			
+			Commit();
+		
+			return Task.FromResult(true);
+		}
+	}
+}
